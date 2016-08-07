fe1fb95316df5793f5be86b2eb85997aa36feeac
pmmlabs <kilowatt36@mail.ru>
2016-08-07 23:15:15 +0300

    toblob polyfill

--- script.js
+++ script.js
@@ -29,6 +29,21 @@ function onError() {
     alert('Ошибка загрузки! Попробуйте файл поменьше.');
 }
 
+if (!HTMLCanvasElement.prototype.toBlob) {
+    Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+        value: function (callback, type, quality) {
+            var binStr = atob( this.toDataURL(type, quality || 0.6).split(',')[1] ),
+                len = binStr.length,
+                arr = new Uint8Array(len);
+
+            for (var i=0; i<len; i++ ) {
+                arr[i] = binStr.charCodeAt(i);
+            }
+            callback( new Blob( [arr], {type: type || 'image/jpeg'} ) );
+        }
+    });
+}
+
 $(function () {
     $.get(PROXY + endpoints['list'], function (_filters) {
         filters = _filters;
